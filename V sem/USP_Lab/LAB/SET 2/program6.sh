6640922fe1455832ba658763c25ddf3b711caadf
AjithKumarG <53942428+Ajith-Kumar-G@users.noreply.github.com>
2021-12-06 07:55:29 +0530

    Update program6.sh

--- V sem/USP_Lab/LAB/SET 2/program6.sh
+++ V sem/USP_Lab/LAB/SET 2/program6.sh
@@ -4,3 +4,4 @@ DA=`echo "scale=3;$bS * 10 / 100"|bc`
 hra=`echo "scale=3;$bS * 20 / 100"|bc`
 gross_salary=`echo "scale=3;$bS + $DA + $hra" |bc`
 echo "The Gross salary is $gross_salary"
+echo "Others: Da=$DA and HRA=$hra"
