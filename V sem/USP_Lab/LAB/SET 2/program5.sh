911cd5c95f6354cb1fa857bf78a6bcc30de09f3e
AjithKumarG <53942428+Ajith-Kumar-G@users.noreply.github.com>
2021-12-06 07:54:26 +0530

    Update program5.sh

--- V sem/USP_Lab/LAB/SET 2/program5.sh
+++ V sem/USP_Lab/LAB/SET 2/program5.sh
@@ -1,6 +1,11 @@
-echo 'Enter the Positive Number'
+echo 'Enter a Number'
 fact=1
 read n
+if test $n -lt 0
+then
+  echo "$n should be Whole number!!"
+  exit 1
+fi
 for (( i=1; i<=$n; i++ ))
 do
 fact=$((fact*i))
